abfe849ce6dd2a506ad39e2a1a8761a03fd3c5ff
mizza-cz <mizza.cz666@gmail.com>
2025-03-27 12:49:23 +0100

    add matcheslive

--- src/components/infoBoard/infoBoard.js
+++ src/components/infoBoard/infoBoard.js
@@ -1,15 +1,15 @@
 $(".matchesAfter").slick({
-   slidesToShow: 4,
-   arrows: true,
-   infinite: false,
-   dots: false,
-   nextArrow:
-     ' <button class="matchesLive__btn  matchesLive__btnnext"><img src="images/content/arrow-right.png" alt="right" loading="lazy" /></button>',
-   prevArrow:
-     ' <button class="matchesLive__btn  matchesLive__btnprev"><img src="images/content/arrow-left.png" alt="left" loading="lazy" /></button>',
-   responsive: [
-     { breakpoint: 1100, settings: { slidesToShow: 3 } },
-     { breakpoint: 840, settings: { slidesToShow: 2 } },
-   ],
- });
- 
+  slidesToShow: 4,
+  arrows: true,
+  infinite: false,
+  dots: false,
+  nextArrow:
+    ' <button class="matchesLive__btn  matchesLive__btnnext"><img src="images/content/arrow-right.png" alt="right" loading="lazy" /></button>',
+  prevArrow:
+    ' <button class="matchesLive__btn  matchesLive__btnprev"><img src="images/content/arrow-left.png" alt="left" loading="lazy" /></button>',
+  responsive: [
+    { breakpoint: 1100, settings: { slidesToShow: 3 } },
+    { breakpoint: 840, settings: { slidesToShow: 2 } },
+    { breakpoint: 540, settings: { slidesToShow: 1 } },
+  ],
+});
