9d505ff3e2651aae2aec6c7bee6f90a9f968175d
mizza-cz <mizza.cz666@gmail.com>
2025-04-08 13:08:05 +0200

    fixes

--- src/components/videoSlider/videoSlider.js
+++ src/components/videoSlider/videoSlider.js
@@ -1,17 +1,16 @@
 $(".videoSlider__inner").slick({
-  slidesToShow: 3,
+  slidesToShow: 4,
   arrows: true,
-  infinite: false,
+  infinite: true,
   dots: false,
-  variableWidth: true,
   nextArrow:
     ' <button class="matchesLive__btn  matchesLive__btnnext"><img src="images/content/arrow-right.png" alt="right" loading="lazy" /></button>',
   prevArrow:
     ' <button class="matchesLive__btn  matchesLive__btnprev"><img src="images/content/arrow-left.png" alt="left" loading="lazy" /></button>',
   responsive: [
-    { breakpoint: 1100, settings: { slidesToShow: 2 } },
-    { breakpoint: 840, settings: { slidesToShow: 2, variableWidth: false } },
+    { breakpoint: 1340, settings: { slidesToShow: 3 } },
+    { breakpoint: 840, settings: { slidesToShow: 2 } },
 
-    { breakpoint: 480, settings: { slidesToShow: 1, variableWidth: false } },
+    { breakpoint: 480, settings: { slidesToShow: 1 } },
   ],
 });
