4b194911f8dc9b427c1186d414edf706f99f7e50
mizza-cz <mizza.cz666@gmail.com>
2026-08-05 11:16:42 +0200

    fix event slider

--- src/components/events-section/events-section.js
+++ src/components/events-section/events-section.js
@@ -1,15 +1,77 @@
-$(".eventsSlider").slick({
-  slidesToShow: 3,
-  arrows: true,
-  infinite: false,
-  dots: false,
-  nextArrow:
-    ' <button class="matchesLive__btn  matchesLive__btnnext"><img src="images/content/arrow-right.png" alt="right" loading="lazy" /></button>',
-  prevArrow:
-    ' <button class="matchesLive__btn  matchesLive__btnprev"><img src="images/content/arrow-left.png" alt="left" loading="lazy" /></button>',
-  responsive: [
-    { breakpoint: 840, settings: { slidesToShow: 2 } },
+// $(".eventsSlider").slick({
+//   slidesToShow: 3,
+//   arrows: true,
+//   infinite: false,
+//   dots: false,
+//   nextArrow:
+//     ' <button class="matchesLive__btn  matchesLive__btnnext"><img src="images/content/arrow-right.png" alt="right" loading="lazy" /></button>',
+//   prevArrow:
+//     ' <button class="matchesLive__btn  matchesLive__btnprev"><img src="images/content/arrow-left.png" alt="left" loading="lazy" /></button>',
+//   responsive: [
+//     { breakpoint: 840, settings: { slidesToShow: 2 } },
 
-    { breakpoint: 480, settings: { slidesToShow: 1 } },
-  ],
+//     { breakpoint: 480, settings: { slidesToShow: 1 } },
+//   ],
+// });
+$(".eventsSlider").each(function () {
+  const $slider = $(this);
+  const slidesCount = $slider.children("li").length;
+
+  if (slidesCount <= 1) {
+    $slider.addClass("eventsSlider--single");
+    return;
+  }
+
+  $slider.slick({
+    slidesToShow: Math.min(3, slidesCount),
+    slidesToScroll: 1,
+    arrows: slidesCount > 3,
+    infinite: false,
+    dots: false,
+
+    nextArrow: `
+      <button
+        type="button"
+        class="matchesLive__btn matchesLive__btnnext"
+        aria-label="Next slide"
+      >
+        <img
+          src="images/content/arrow-right.png"
+          alt=""
+          loading="lazy"
+        />
+      </button>
+    `,
+
+    prevArrow: `
+      <button
+        type="button"
+        class="matchesLive__btn matchesLive__btnprev"
+        aria-label="Previous slide"
+      >
+        <img
+          src="images/content/arrow-left.png"
+          alt=""
+          loading="lazy"
+        />
+      </button>
+    `,
+
+    responsive: [
+      {
+        breakpoint: 840,
+        settings: {
+          slidesToShow: Math.min(2, slidesCount),
+          arrows: slidesCount > 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          arrows: slidesCount > 1,
+        },
+      },
+    ],
+  });
 });
